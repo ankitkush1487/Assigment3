0b95967a8aa07b09b395922cdf5dc8196768bcbc
Ankit kumar kushwaha <ankitkush6207@gmail.com>
2024-09-17 15:43:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment3
+{
+    public class Employee
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+        public Employee(String name, int age, int salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Name:{Name}");
+            Console.WriteLine($"Age:{Age}");
+            Console.WriteLine($"Salary:{Salary}");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Ankit kumar",24,5000000);
+            employee1.Display();
+            Console.ReadLine();
+        }
+    }
+}
